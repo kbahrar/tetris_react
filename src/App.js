ca7ab95ef92a0ac4bbbd51603f97307a83410f3e
kbahrar <kamalbahrar99@gmail.com>
2022-01-03 18:02:21 +0100

    add grid board

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import React from 'react';
 
-import GridSquare from './components/GridSquare';
+import GridBoard from './components/GridBoard';
 
 function App() {
   return (
@@ -9,7 +9,7 @@ function App() {
       <header className="App-header">
         <h1 className="App-title">Tetris</h1>  
       </header>
-      <GridSquare color="1" />
+      <GridBoard />
     </div>
   );
 }
