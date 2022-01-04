48ab50e2f0a8c3a9a591f3349a22ad0c3c7781a5
kbahrar <kamalbahrar99@gmail.com>
2022-01-04 19:19:59 +0100

    add connection for grid board

--- src/components/GridBoard.js
+++ src/components/GridBoard.js
@@ -1,19 +1,33 @@
 import React from "react";
 import GridSquare from "./GridSquare";
+import {useSelector} from 'react-redux'
+import { shapes } from "../utils";
 
 export default function GridBoard(props) {
-    const grid = [];
+    const game = useSelector((state) => state.game)
+    const { grid, shape, rotation, x, y, isRunning, speed } = game
 
-    for (let row = 0; row < 20; row++) {
-        grid.push([])
-        for (let col = 0; col < 10; col++) {
-            grid[row].push(<GridSquare key={`${col}${row}`} color="1" />)
-        }
-    }
+    const block = shapes[shape][rotation]
+    const blockColor = shape
+
+    const gridSquares = grid.map((rowArray, col) => {
+        return rowArray.map((square, col) => {
+            const blockX = col - x
+            const blockY = row - y
+            let color = square
+            if (blockX >= 0 && blockX < block.length && blockY >= 0 && blockY >= block.length) {
+                color = block[blockY][blockX] === 0 ? color : blockColor
+            }
+            const k = row * grid[0].length + col
+            return <GridSquare
+                        key={k}
+                        color={color} />
+        })
+    })
 
     return (
         <div className='grid-board'>
-            {grid}
+            {gridSquares}
         </div>
     )
 }
