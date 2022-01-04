6135d5830da2ad1f6f5539f865f1c7e720adf847
kbahrar <kamalbahrar99@gmail.com>
2022-01-04 16:36:13 +0100

    add redux store and state utils

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import './App.css';
 import React from 'react';
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import reducers from './reducers';
 
 import GridBoard from './components/GridBoard';
 import NextBlock from './components/NextBlock';
@@ -7,18 +10,22 @@ import ScoreBoard from './components/ScoreBoard';
 import Controls from './components/Controls';
 import MessagePopup from './components/MessagePopup';
 
+const store = createStore(reducers)
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1 className="App-title">Tetris</h1>  
-      </header>
-      <GridBoard />
-      <NextBlock />
-      <ScoreBoard />
-      <Controls />
-      <MessagePopup />
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Tetris</h1>  
+        </header>
+        <GridBoard />
+        <NextBlock />
+        <ScoreBoard />
+        <Controls />
+        <MessagePopup />
+      </div>
+    </Provider>
   );
 }
 
