cd4e14d2392ef9391320ccaa13d62587d67f2284
kbahrar <kamalbahrar99@gmail.com>
2022-01-05 16:24:56 +0100

    add connection to scoreBoard and messagePopup

--- src/components/MessagePopup.js
+++ src/components/MessagePopup.js
@@ -1,10 +1,29 @@
 import React from "react";
+import { useSelector } from 'react-redux'
 
 export default function MessagePopup(props) {
+    const isRunning = useSelector((state) => state.game.isRunning)
+    const gameOver = useSelector((state) => state.game.gameOver)
+    var classname = 'message-popup'
+    var msg = {title: '', message: ''}
+
+    if (isRunning && !gameOver)
+        classname = 'message-popup-hidden'
+    
+    if (!isRunning) {
+        msg.title = 'Paused'
+        msg.message = 'You can resume your game by click in resume button'
+    }
+
+    if (gameOver) {
+        msg.title = 'Game Over'
+        msg.message = 'You can restart your game by click in play button'
+    }
+    
     return (
-        <div className='message-popup'>
-            <h1>Message Title</h1>
-            <p>Message Info ...</p>
+        <div className={classname}>
+            <h1>{msg.title}</h1>
+            <p>{msg.message}</p>
         </div>
     )
 }
