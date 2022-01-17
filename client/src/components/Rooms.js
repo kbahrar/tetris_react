2a3d6616d62e4950ccb976c8efd9990a506c75ef
kbahrar <kamalbahrar99@gmail.com>
2022-01-17 17:07:05 +0100

    add buttons new msg and new room and improve design

--- client/src/components/Rooms.js
+++ client/src/components/Rooms.js
@@ -4,7 +4,7 @@ import { useState } from "react";
 // import { useDispatch, useSelector } from 'react-redux'
 
 export default function Rooms(props) {
-    const [users, setUsers] = useState(["kamal", "lamak", "opsa", "kola", "koka"])
+    const [users, setUsers] = useState(["kamal", "lamak", "opsa", "kola", "hohala"])
 
     return (
         <>
@@ -20,19 +20,28 @@ export default function Rooms(props) {
                 <div className="header-online">
                     Rooms
                 </div>
-                {users.map((item, index) => (
-                    <div key={index} className="user-online">{item} <div key={"button-" + index} className="join-room">JOIN</div> </div>
-                ))}
+                <div className="rooms-group">
+                    {users.map((item, index) => (
+                        <div key={index} className="user-online">{item} <div key={"button-" + index} className="join-room">JOIN</div> </div>
+                    ))}
+                </div>
+                <div className="form-room"><input type="text" className="input-room" /> <div className="button-room">CREATE ROOM</div> </div>
             </div>
             <div className="chat">
                 <div className="header-online">
                     chat
                 </div>
-                {users.map((item, index) => (
-                    <>
-                        <fieldset key={index} className="message"><legend className="message-sender">{item}</legend>{item}</fieldset>
-                    </>
-                ))}
+                <div className="chat-group">
+                    {users.map((item, index) => (
+                        <>
+                            <div className="message" key={index}>
+                                <span className="message-sender">{item}
+                                </span> : {item}
+                            </div>
+                        </>
+                    ))}
+                </div>
+                <div className="form-room"><input type="text" className="input-room" /> <div className="button-room">SEND</div> </div>
             </div>
         </>
     )
